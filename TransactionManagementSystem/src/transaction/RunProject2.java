c96ed8d14a55769af19ced3edce04023bc3c705e
Taranvir <taranvirsingh97@gmail.com>
2020-10-03 06:26:46 -0400

    "First Commit"

---
+++ TransactionManagementSystem/src/transaction/RunProject2.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package transaction;
+
+/**
+ * @author Taranvir
+ *
+ */
+public class RunProject2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
